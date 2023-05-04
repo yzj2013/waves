2d70d518917b8038455efa18b298539225c392e8
jennyyang <yzhenjuan@gmail>
2023-05-04 23:13:06 +1000

    Finished user email

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,34 +1,4 @@
-// const { userService } = require('../services');
-// const httpStatus = require('http-status');
-// const { ApiError } = require('../middleware/apiError');
-
-// const usersController = {
-//   async profile(req, res, next) {
-//     try {
-//       const user = await userService.findUserById(req.user._id);
-//       if (!user) {
-//         throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
-//       }
-
-//       res.json(res.locals.permission.filter(user._doc));
-//     } catch (error) {
-//       next(error);
-//     }
-//   },
-
-//   async updateProfile(req, res, next) {
-//     try {
-//       const user = await userService.updateUserProfile(req);
-//       res.json(user); ///. filter response fields
-//     } catch (error) {
-//       next(error);
-//     }
-//   },
-// };
-
-// module.exports = usersController;
-
-const { userService } = require('../services');
+const { userService, authService } = require('../services');
 const httpStatus = require('http-status');
 const { ApiError } = require('../middleware/apiError');
 
@@ -39,15 +9,32 @@ const usersController = {
       if (!user) {
         throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
       }
+
       res.json(res.locals.permission.filter(user._doc));
     } catch (error) {
       next(error);
     }
   },
+
   async updateProfile(req, res, next) {
     try {
       const user = await userService.updateUserProfile(req);
-      res.json(user); ///. filter response fields
+      res.json(user); // filter response fields
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async updateUserEmail(req, res, next) {
+    try {
+      const user = await userService.updateUserEmail(req);
+      const token = await authService.genAuthToken(user);
+
+      // send email to vertify account
+      res.cookie('x-access-token', token).send({
+        user,
+        token,
+      });
     } catch (error) {
       next(error);
     }
