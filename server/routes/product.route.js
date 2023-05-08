b54ae0e7a2f8761a0a038a252707248ce87a335a
jennyyang <yzhenjuan@gmail>
2023-05-08 23:42:48 +1000

    Using express validator

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/products.controller');
 const auth = require('../middleware/auth');
+const { addProductValidator } = require('../middleware/validations');
 
-router.post('/', auth('createAny', 'product'), productsController.addProduct);
+router.post(
+  '/',
+  auth('createAny', 'product'),
+  addProductValidator,
+  productsController.addProduct
+);
 
 module.exports = router;
