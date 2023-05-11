b7bdd98998af832037976992b915d5d1096d2f79
jennyyang <yzhenjuan@gmail>
2023-05-11 10:50:16 +1000

    Prepare client and routes

---
+++ client/src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Header from 'components/navigation/header';
+import Footer from 'components/navigation/footer';
+
+import Home from 'components/home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
